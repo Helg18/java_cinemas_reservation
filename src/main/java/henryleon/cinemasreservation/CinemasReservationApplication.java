7d8182d51d127cbac5ec9307805359bc717d4722
Henry Leon Gomez <helg18@gmail.com>
2019-07-24 20:10:27 -0400

    First Commit

---
+++ src/main/java/henryleon/cinemasreservation/CinemasReservationApplication.java
@@ -0,0 +1,13 @@
+package henryleon.cinemasreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemasReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CinemasReservationApplication.class, args);
+    }
+
+}
