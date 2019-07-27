12d239c6cd1f4a214a2c4533ce0e43fe7a9e0d60
Henry Leon <helg18@gmail.com>
2019-07-27 16:02:24 -0400

    Updated SQL

--- src/main/java/henryleon/cinemasreservation/Databass/0001-create-rooms-table.sql
+++ src/main/java/henryleon/cinemasreservation/Databass/0001-create-rooms-table.sql
@@ -3,8 +3,8 @@ create table rooms
 	id BIGINT auto_increment,
 	name varchar(300) not null,
 	status boolean default TRUE not null,
-	created_at timestamp null,
-	updated_at timestamp null,
+    created_at datetime default now() null,
+    updated_at datetime default now() null,
 	constraint rooms_pk
 		primary key (id)
 );
