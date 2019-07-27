0aa1d7bc0a8388d70e9d5af51fcf9eebe70fff29
Henry Leon Gomez <helg18@gmail.com>
2019-07-27 01:22:19 -0400

    Update Seats

--- src/main/java/henryleon/cinemasreservation/Controllers/SeatController.java
+++ src/main/java/henryleon/cinemasreservation/Controllers/SeatController.java
@@ -28,4 +28,19 @@ public class SeatController {
     public Optional<Seat> show(@PathVariable Long id) {
         return this.seatRepository.findById(id);
     }
+
+    @RequestMapping(value = "/update/{id}", method = RequestMethod.PUT)
+    public List update(@PathVariable Long id, @RequestBody Seat newSeat) {
+        Optional<Seat> optionalSeat = this.seatRepository.findById(id);
+        if (optionalSeat.isPresent()) {
+            Seat seat = optionalSeat.get();
+            seat.setCol(newSeat.getCol());
+            seat.setRow(newSeat.getRow());
+            seat.setRoom(newSeat.getRoom());
+            seat.setStatus(newSeat.getStatus());
+
+            this.seatRepository.save(seat);
+        }
+        return this.seatRepository.findAll();
+    }
 }
