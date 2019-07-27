fa1bb1f1d357484b42cc270532626f3ca5d9cb7a
Henry Leon Gomez <helg18@gmail.com>
2019-07-27 02:01:02 -0400

    Add SQL file to create reservations table

---
+++ src/main/java/henryleon/cinemasreservation/Databass/0003-create_reservations-table.sql
@@ -0,0 +1,12 @@
+create table reservations
+(
+    id bigint auto_increment,
+    date date not null,
+    time time not null,
+    status boolean default true not null,
+    created_at datetime default now() null,
+    updated_at datetime null,
+    constraint reservations_pk
+        primary key (id)
+);
+
